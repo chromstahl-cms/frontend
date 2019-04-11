1992196fb1d18022fce3dc5acdc6875295bd67e3
fred <fredlahde@gmail.com>
2019-04-11 22:45:34 +0200

    Cypress: Add more router tests

--- cypress/integration/routerTest.js
+++ cypress/integration/routerTest.js
@@ -1,13 +1,21 @@
 /// <reference types="Cypress" />
 
 context('Assertions', () => {
-    beforeEach(() => {
-        cy.visit('/');
-    });
-
     describe("router-test", () => {
         it('.should route to login', () => {
-            cy.get('.loginIcon').click().get('h1').should("have.text", "Log in");
+            cy.visit('/').get('.loginIcon').click().get('h1').should("have.text", "Log in");
+        });
+
+        it('.it should navigate back to home from login', () => {
+            cy.visit('/login').get('#target > div:nth-child(2) > div > a:nth-child(1) > h2').click().get('#blogMount');
+        });
+
+        it('.it should navigate to register from login', () => {
+            cy.visit('/login').get('.register-link').click().get('span').contains('Register');
+        });
+
+        it('.it should navigate to login from register', () => {
+            cy.visit('/register').get('.login-link').click().get('span').contains('Login');
         });
     });
 });
